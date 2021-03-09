a42f78f9664f0699bdb9035d7a294c2843cd4ea1
DaBluLite <dabluliteplays@gmail.com>
2021-03-09 16:08:53 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,16 +3,17 @@
 #                                                                                #
 #                                By DaBluLite                                    #
 #                                                                                #
-#            Note: you must know the names of the main binary                    #
-#                       executable and the icon file.                            #
+#            Note: you must know the name of the icon file.                      #
 #                                                                                #
 ##################################################################################
-APP_NAME=<Your_app_name_here> #This will be the main app name. It will be displayed in the app launcher and in the desktop shortcut
-APP_PATH_NAME=<Your_app_path_name_here> #This will be the name for the paths, the AppImage and others. MUST BE URL-FRIENDLY!
+echo -en "\033]0;Your AppImage Installer Name\a"
+APP_NAME=Your App #This will be the main app name. It will be displayed in the app launcher and in the desktop shortcut
+APP_PATH_NAME=your-app #This will be the name for the paths, the AppImage and others. MUST BE URL-FRIENDLY!
+APP_URL=http://your-app.url.com/your-app.appimage #This must lead to an appimage file
 SUDO_USER=$(logname)
-mkdir /opt/google-assistant
-cd /opt/google-assistant
-wget -r -l 1 --span-hosts --output-document=$APP_PATH_NAME.AppImage -erobots=off -nH <Your_appimage_url> #This must lead to an appimage file
+mkdir /opt/$APP_PATH_NAME
+cd /opt/$APP_PATH_NAME
+wget -r -l 1 --span-hosts --output-document=$APP_PATH_NAME.AppImage -erobots=off -nH $APP_URL
 chmod +xwr $APP_PATH_NAME.AppImage
 chown $SUDO_USER $APP_PATH_NAME.AppImage
 ./$APP_PATH_NAME.AppImage --appimage-extract
@@ -29,23 +30,21 @@ Name=$APP_NAME
 Exec=/opt/$APP_PATH_NAME/AppRun
 Terminal=false
 Type=Application
-Icon=/opt/$APP_PATH_NAME/<Your_icon_name>
+Icon=/opt/$APP_PATH_NAME/your-app-icon.png
 StartupWMClass=$APP_NAME
 X-AppImage-Version=2.5.0
-Comment=Your app description goes here
+Comment=$APP_NAME
 Categories=Utility;
 EOT
 chmod 775 /home/*/Desktop/$APP_PATH_NAME.desktop
 chmod +x /home/*/Desktop/$APP_PATH_NAME.desktop
 chown $SUDO_USER /home/*/Desktop/$APP_PATH_NAME.desktop
 cd /opt/google-assistant
+chmod +xwr /opt/$APP_PATH_NAME -R
 chmod 775 /opt/$APP_PATH_NAME -R
 chown $SUDO_USER /opt/$APP_PATH_NAME -R
 chmod 775 /opt/$APP_PATH_NAME/AppRun
 chmod +rwx /opt/$APP_PATH_NAME/AppRun
 chown $SUDO_USER /opt/$APP_PATH_NAME/AppRun
-chmod 775 /opt/$APP_PATH_NAME/<Your_app_binary_executable>
-chmod +rwx /opt/$APP_PATH_NAME/<Your_app_binary_executable>
-chown $SUDO_USER /opt/$APP_PATH_NAME/<Your_app_binary_executable>
 cd /home/*/Desktop
 cp $APP_PATH_NAME.desktop /usr/share/applications
